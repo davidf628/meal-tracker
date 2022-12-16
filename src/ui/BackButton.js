1358b3180578454296d3ffb27a04059f83fd09db
David Flenner <30780186+davidf628@users.noreply.github.com>
2022-12-16 09:42:39 -0500

    New files added

--- src/ui/BackButton.js
+++ src/ui/BackButton.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { useHistory } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 export const BackButton = () => {
-    const history = useHistory();
+    const history = useNavigate();
 
     return (
         <button className="back-button" onClick={history.goBack}>Back</button>
